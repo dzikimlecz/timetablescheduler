38a45e9c2c9a6cb07c619f65eb564df78ae2e3bc
dzikimlecz <j.pul312@gmail.com>
2021-02-15 10:21:35 +0100

    modified showTimeTableSetUp to display dialog instead of using main stage.

--- src/main/kotlin/me/dzikimlecz/managers/WindowsManager.kt
+++ src/main/kotlin/me/dzikimlecz/managers/WindowsManager.kt
@@ -1,21 +1,19 @@
-package me.dzikimlecz
+package me.dzikimlecz.managers
 
+import javafx.event.EventHandler
 import javafx.geometry.Pos
 import javafx.scene.Scene
-import javafx.scene.control.Button
-import javafx.scene.control.Label
-import javafx.scene.control.TextField
-import javafx.scene.control.TextFormatter
+import javafx.scene.control.*
 import javafx.scene.layout.GridPane
 import javafx.scene.text.Font
 import javafx.stage.Stage
 import java.util.*
 
-class ScenesManager(private val stage: Stage) {
-
+class WindowsManager(private val mainStage: Stage) {
+    private val dialogStage = Stage()
 
     fun showTimeTableSetUp(properties: Properties) {
-        stage.title = "Nowy Plan"
+        dialogStage.title = "Nowy Plan"
         val pane = GridPane()
         pane.hgap = 10.0
         pane.vgap = 10.0
@@ -40,17 +38,24 @@ class ScenesManager(private val stage: Stage) {
         val proceedButton = Button("Ok")
         proceedButton.font = font
 
+        proceedButton.onAction = EventHandler {
+            var text = columnsField.text
+            properties.setProperty("ColumnsAmount", if (text.isNotEmpty()) text else "1")
+            text = rowsField.text
+            properties.setProperty("RowsAmount", if (text.isNotEmpty()) text else "1")
+            dialogStage.close()
+        }
+
         pane.add(infoLabel, 0, 0, 5, 1)
         pane.addRow(1, columnsLabel, columnsField)
         pane.addRow(2, rowsLabel, rowsField)
         pane.add(proceedButton, 5, 2, 2, 1)
 
-        stage.scene = Scene(pane)
-        stage.height = 180.0
-        stage.width = 400.0
-        stage.centerOnScreen()
-        stage.isResizable = false
-        stage.show()
+        dialogStage.scene = Scene(pane, 400.0, 180.0)
+        dialogStage.sizeToScene()
+        dialogStage.centerOnScreen()
+        dialogStage.isResizable = false
+        dialogStage.showAndWait()
         proceedButton.requestFocus()
     }
 
