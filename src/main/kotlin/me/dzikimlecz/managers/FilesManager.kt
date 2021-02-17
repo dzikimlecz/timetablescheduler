5eeb79102fbc069a56f9d7316e0d822ce7720069
dzikimlecz <j.pul312@gmail.com>
2021-02-17 08:40:06 +0100

    Added class for saving

---
+++ src/main/kotlin/me/dzikimlecz/managers/FilesManager.kt
@@ -0,0 +1,76 @@
+package me.dzikimlecz.managers
+
+import kotlinx.serialization.json.Json
+import me.dzikimlecz.timetable.TimeTable
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+
+class FilesManager(
+    val defaultSavePath: String = "${System.getProperty("APPDATA")}\\TimeTableScheduler"
+) {
+    init {
+        val file = File(defaultSavePath)
+        if (!file.exists()) file.mkdirs()
+    }
+
+
+    fun saveTable(timeTable: TimeTable, path: String = defaultSavePath) {
+        val filename = (if (timeTable.name.isBlank())
+            timeTable.dateCreated.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT))
+                .replace(Regex("[-:.]"), "_")
+        else timeTable.name) + ".json"
+        val file = File(path, filename)
+        val resultStatus = if (file.exists()) checkIdentity(file, timeTable)
+        else serialize(timeTable, file)
+        when (resultStatus) {
+            0 -> return
+            1 -> serialize(timeTable, file)
+            -1 -> throw IOException("Brak dostępu do pliku")
+            2 -> throw FileAlreadyExistsException(file,
+                reason = "Ten plik może zawierać już inny plan!")
+            3 -> throw IOException("Błąd zapisu")
+            else -> throw IOException()
+        }
+    }
+
+    private fun serialize(timeTable: TimeTable, file: File) : Int {
+        val serialized = try {
+            Json.encodeToString(TimeTable.serializer(), timeTable)
+        } catch (e: Exception) {
+            return 3
+        }
+        if (!file.exists()) file.createNewFile()
+        return if (!file.canWrite()) -1 else try {
+            file.writeText(serialized)
+            0
+        } catch (e: Exception) {
+            3
+        }
+    }
+
+    private fun checkIdentity(file: File, timeTable: TimeTable) : Int {
+        require(file.exists())
+        if (!file.canRead()) return if (file.canWrite()) 2 else -1
+        val serialized = file.readText()
+        val table = Json.decodeFromString(TimeTable.serializer(), serialized)
+        return if (table.name.equals(timeTable.name, true) ||
+            table.dateCreated == timeTable.dateCreated
+        ) 1 else 2
+    }
+
+
+    fun readTable(name: String, path: String = defaultSavePath) : TimeTable {
+        val file = File(path, "$name.json")
+        if (!file.exists()) throw FileNotFoundException("Plik \"$file\" nie istnieje.")
+        if (!file.canRead()) throw IOException("Odczyt pliku zablokowany przez system")
+        return try {
+            Json.decodeFromString(TimeTable.serializer(), file.readText())
+        } catch (e: Exception) {
+            throw IOException("Zawartość pliku została naruszona. Odczyt niemożliwy", e)
+        }
+    }
+
+}
