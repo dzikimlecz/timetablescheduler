39860821e40859a5ea0da02900c2c20ca5817da9
dzikimlecz <j.pul312@gmail.com>
2021-02-19 18:37:45 +0100

    added Class for managing tables

---
+++ src/main/kotlin/me/dzikimlecz/timetables/managers/TablesManager.kt
@@ -0,0 +1,13 @@
+package me.dzikimlecz.timetables.managers
+
+import java.util.concurrent.atomic.AtomicInteger
+
+class TablesManager {
+
+
+
+    fun getNewTimeTable(x: Int, y: Int) {
+
+    }
+
+}
