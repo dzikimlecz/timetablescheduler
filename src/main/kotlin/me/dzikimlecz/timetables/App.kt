dc1306d54705c3decad0826b8896f62379145434
dzikimlecz <j.pul312@gmail.com>
2021-03-11 13:31:44 +0100

    simplified imports

--- src/main/kotlin/me/dzikimlecz/timetables/App.kt
+++ src/main/kotlin/me/dzikimlecz/timetables/App.kt
@@ -1,14 +1,13 @@
 package me.dzikimlecz.timetables
 
 import me.dzikimlecz.timetables.components.views.MainView
-import me.dzikimlecz.timetables.components.views.TimeTableSetUpView
+
 import tornadofx.launch
-import tornadofx.App as Application
 
 fun main() {
     System.setProperty("defaultSavePath", "${System.getenv("APPDATA")}\\TimeTableScheduler")
     launch<App>()
 }
 
-class App : Application(MainView::class)
+class App : tornadofx.App(MainView::class)
 
