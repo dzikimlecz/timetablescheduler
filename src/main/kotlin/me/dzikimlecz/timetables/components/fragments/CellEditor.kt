c279a95f2abc837cb409b04a413f29db937b37c0
dzikimlecz <j.pul312@gmail.com>
2021-03-14 13:49:32 +0100

    divided refreshView and its implementation

--- src/main/kotlin/me/dzikimlecz/timetables/components/fragments/CellEditor.kt
+++ src/main/kotlin/me/dzikimlecz/timetables/components/fragments/CellEditor.kt
@@ -2,15 +2,18 @@ package me.dzikimlecz.timetables.components.fragments
 
 import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.property.SimpleStringProperty
+import javafx.scene.control.Control
 import javafx.scene.control.Label
-import javafx.scene.control.TextField
+import javafx.scene.paint.Color
+import me.dzikimlecz.timetables.components.fragments.TimeTableEditor.ViewMode
+import me.dzikimlecz.timetables.components.fragments.TimeTableEditor.ViewMode.VIEW
 import me.dzikimlecz.timetables.timetable.Cell
 import tornadofx.*
-import java.util.logging.Level
-import java.util.logging.Logger
+import tornadofx.Dimension.LinearUnits.px
+import java.lang.Double.max
 
 class CellEditor : Fragment() {
-    val cell : Cell by param()
+    val cell: Cell by param()
     private val divisionProperty = SimpleBooleanProperty()
     private val texts = Array(2) { SimpleStringProperty() }
 
@@ -23,25 +26,45 @@ class CellEditor : Fragment() {
         for ((i, e) in texts.withIndex())
             e.bindBidirectional(cell.getContentProperty(i))
         divisionProperty.bind(cell.isDivided)
-        divisionProperty.addListener { _ -> refreshView(TimeTableEditor.ViewMode.EDIT) }
+        divisionProperty.addListener { _ -> refreshView(ViewMode.EDIT) }
     }
 
-    fun refreshView(viewMode: TimeTableEditor.ViewMode) {
-        val nodes = { i: Int ->
-            if (viewMode == TimeTableEditor.ViewMode.VIEW) label {
-                paddingAll = 10
-                textProperty().bindBidirectional(texts[i])
-                minWidthProperty().bind(root.minWidthProperty() + 5)
-                minHeightProperty().bind(root.minHeightProperty() + 25)
-            } else textarea {
-                textProperty().bindBidirectional(texts[i])
-            }
+
+
+    fun refreshView(viewMode: ViewMode) = refreshImpl {
+        if (viewMode == VIEW) label {
+            paddingAll = 10
+            textProperty().bindBidirectional(texts[it])
+            minWidth = root.minWidth + 5
+            minHeight = root.minHeight + 25
+        } else textarea {
+            textProperty().bindBidirectional(texts[it])
         }
+    }
+
+    private inline fun refreshImpl(nodes: (i: Int) -> Control) {
         with(root) {
             clear()
             if (divisionProperty.get()) {
                 left = nodes(0)
                 right = nodes(1)
+                if (left is Label) with(left) {
+                    style {
+                        borderColor += CssBox(
+                            Color.TRANSPARENT,
+                            Color.DARKGREY,
+                            Color.TRANSPARENT,
+                            Color.TRANSPARENT
+                        )
+                        val dim = Dimension(.0, px)
+                        borderWidth += CssBox(dim, Dimension(2.0, px), dim, dim)
+                    }
+                }
+                val leftControl = left as Control
+                val rightControl = right as Control
+                leftControl.prefWidthProperty().bind(root.widthProperty() / 2)
+                rightControl.prefWidthProperty().bind(root.widthProperty() / 2)
+                minHeight = max(leftControl.minHeight, rightControl.minHeight)
             } else center = nodes(0)
         }
     }
