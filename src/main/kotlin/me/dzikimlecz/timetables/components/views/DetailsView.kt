c3b96de40e5a41c59eb3fb367e387cb3782303e2
dzikimlecz <j.pul312@gmail.com>
2021-03-14 20:24:22 +0100

    introduced view for editing timetable's details

---
+++ src/main/kotlin/me/dzikimlecz/timetables/components/views/DetailsView.kt
@@ -0,0 +1,61 @@
+package me.dzikimlecz.timetables.components.views
+
+import me.dzikimlecz.timetables.timetable.TimeTable
+import tornadofx.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import kotlin.reflect.KProperty1
+
+class DetailsView : View("Szczegóły Planu") {
+    val table by param<TimeTable>()
+    private val tableProperties = mutableMapOf<KProperty1<TimeTable, Any>, String>()
+
+    override val root = form {
+        fieldset("Plan ${table.name}") {
+            field("Nazwa") {
+                textfield {
+                    text = table.name
+                    textProperty().addListener { _, _, newValue ->
+                        tableProperties[TimeTable::name] = newValue
+                    }
+                }
+            }
+            field("Data") {
+                datepicker {
+                    editor.text = table.date.format(DateTimeFormatter.ISO_LOCAL_DATE)
+                    editor.textProperty().addListener { _, _, newValue ->
+                        tableProperties[TimeTable::date] = newValue
+                    }
+                }
+            }
+            field("Kolumny") {
+                textfield {
+                    text = table.columns.toString()
+                    textProperty().addListener { _, _, newValue ->
+                        tableProperties[TimeTable::columns] = newValue
+                    }
+                }
+            }
+            field("Rzędy") {
+                textfield {
+                    text = table.rows.toString()
+                    textProperty().addListener { _, _, newValue ->
+                        tableProperties[TimeTable::rows] = newValue
+                    }
+                }
+            }
+            buttonbar {
+                button("Ok").setOnAction { commit() }
+            }
+        }
+    }
+
+    private fun commit() {
+        table.name = tableProperties[TimeTable::name] ?: table.name
+        table.rows = tableProperties[TimeTable::rows]?.toIntOrNull() ?: table.rows
+        table.columns = tableProperties[TimeTable::columns]?.toIntOrNull() ?: table.columns
+        val dateString = tableProperties[TimeTable::date]
+        if (dateString !== null) table.date = LocalDate.parse(dateString)
+        close()
+    }
+}
