20db43e702646453d3a314336ce8ab6414dcd80a
dzikimlecz <j.pul312@gmail.com>
2021-03-17 11:17:53 +0100

    added divisionDirection property serialization

--- src/main/kotlin/me/dzikimlecz/timetables/timetable/json/CellSerializer.kt
+++ src/main/kotlin/me/dzikimlecz/timetables/timetable/json/CellSerializer.kt
@@ -1,5 +1,6 @@
 package me.dzikimlecz.timetables.timetable.json
 
+import javafx.geometry.Orientation
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -10,7 +11,8 @@ import me.dzikimlecz.timetables.timetable.Cell
 
 @Serializable
 @SerialName("Cell")
-private data class CellSurrogate(val content: List<String>) {
+private data class CellSurrogate(val content: List<String>,
+                                 val divisionOrientation: Orientation?= null) {
     init { require(content.size in 1..2) }
 }
 
@@ -20,13 +22,15 @@ object CellSerializer : KSerializer<Cell> {
     override fun serialize(encoder: Encoder, value: Cell) {
         val content = mutableListOf(value[0])
         if (value.isDivided) content.add(value[1])
-        encoder.encodeSerializableValue(CellSurrogate.serializer(), CellSurrogate(content))
+        encoder.encodeSerializableValue(CellSurrogate.serializer(),
+            CellSurrogate(content, value.divisionDirection))
     }
 
     override fun deserialize(decoder: Decoder): Cell {
         val surrogate = decoder.decodeSerializableValue(CellSurrogate.serializer())
         val cell = Cell(surrogate.content.size == 2)
         for ((i, element) in surrogate.content.withIndex()) cell[i] = element
+        cell.divisionDirection = surrogate.divisionOrientation
         return cell
     }
 }
