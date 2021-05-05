d19cc239a67d0d16821e416b4b043b1150894d65
dzikimlecz <j.pul312@gmail.com>
2021-05-05 18:53:52 +0200

    added serialization of titles

--- src/main/kotlin/me/dzikimlecz/timetables/timetable/Serializers.kt
+++ src/main/kotlin/me/dzikimlecz/timetables/timetable/Serializers.kt
@@ -1,9 +1,9 @@
 package me.dzikimlecz.timetables.timetable
 
+import javafx.beans.property.SimpleStringProperty
 import javafx.collections.FXCollections
 import javafx.geometry.Orientation
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.Required
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.descriptors.PrimitiveKind
@@ -11,7 +11,6 @@ import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
-import me.dzikimlecz.lecturers.LecturerTransferredSurrogate
 import me.dzikimlecz.serializers.DateSerializer
 import tornadofx.sizeProperty
 import java.time.LocalDate
@@ -22,29 +21,36 @@ import javax.naming.OperationNotSupportedException
 object TimeTableSerializer : KSerializer<TimeTable> {
     override val descriptor: SerialDescriptor = TimeTableSurrogate.serializer().descriptor
 
-    override fun serialize(encoder: Encoder, value: TimeTable) {
-        val name = value.name.ifBlank { value.date.format(DateTimeFormatter.ISO_DATE) }
+    override fun serialize(encoder: Encoder, value: TimeTable) = with (value) {
+        val nameToSave = name.ifBlank { date.format(DateTimeFormatter.ISO_DATE) }
         encoder.encodeSerializableValue(
             TimeTableSurrogate.serializer(),
-            TimeTableSurrogate(value.date, name, value.list, value.columnsTimeSpan.map { it.toTypedArray() },)
+            TimeTableSurrogate(date, nameToSave, list, columnsTimeSpan.map { it.toTypedArray()}, titles.map {it.value })
         )
     }
 
     override fun deserialize(decoder: Decoder): TimeTable {
         val surrogate = decoder.decodeSerializableValue(TimeTableSurrogate.serializer())
         val timeTable = timeTableOf(surrogate.table)
-        timeTable.name = surrogate.name
-        timeTable.date = surrogate.date
-        val timeSpans = surrogate.timeSpans
-        if (timeSpans != null) {
-            timeTable.columnsTimeSpan.clear()
-            timeSpans.map {
-                FXCollections.observableArrayList(*it).apply {
-                    sizeProperty.addListener { _, _, _, ->
-                        throw OperationNotSupportedException("Lists of TimeSpans must have fixed size.")
+        with(surrogate) {
+            timeTable.name = name
+            timeTable.date = date
+            if (timeSpans != null) {
+                timeTable.columnsTimeSpan.clear()
+                timeSpans.map {
+                    FXCollections.observableArrayList(*it).apply {
+                        sizeProperty.addListener { _, _, _ ->
+                            throw OperationNotSupportedException("Lists of TimeSpans must have fixed size.")
+                        }
                     }
+                }.also { timeTable.columnsTimeSpan.addAll(it) }
+            }
+            if (titles != null) {
+                timeTable.titles.clear()
+                timeTable.titles += titles.map {
+                    SimpleStringProperty(it)
                 }
-            }.also { timeTable.columnsTimeSpan.addAll(it) }
+            }
         }
         return timeTable
     }
@@ -53,12 +59,22 @@ object TimeTableSerializer : KSerializer<TimeTable> {
     @SerialName("TimeTable")
     private class TimeTableSurrogate(
         @Serializable(with = DateSerializer::class) val date: LocalDate,
-        @Required val name: String = "",
+        val name: String,
         val table: List<List<Cell>>,
-        val timeSpans : List<Array<TimeSpan?>>?= null
+        val timeSpans: List<Array<TimeSpan?>>?= null,
+        val titles: List<String>?= null,
     ) {
         init {
-            require(table.stream().allMatch { it.size == table[0].size }) {"This list is not a table!"}
+            if (table.isNotEmpty()) {
+                val size = table[0].size
+                require(table.stream().allMatch { it.size == size }) { "This list is not a table!" }
+                require((timeSpans?.size ?: size) == size) {
+                    "Size of time spans: ${timeSpans?.size} doesn't match size of the table: $size)"
+                }
+                require((titles?.size ?: size) == size) {
+                    "Size of titles: ${titles?.size} doesn't match size of the table: $size)"
+                }
+            }
         }
     }
 }
