f2ec5cb7ae8cbbe3ff4303c7c06d0af6b708fb56
dzikimlecz <j.pul312@gmail.com>
2021-05-19 21:23:17 +0200

    fixed bug of improper date filtering

--- src/main/kotlin/me/dzikimlecz/timetables/components/views/LecturerWorkTimeDisplay.kt
+++ src/main/kotlin/me/dzikimlecz/timetables/components/views/LecturerWorkTimeDisplay.kt
@@ -3,10 +3,12 @@ package me.dzikimlecz.timetables.components.views
 import javafx.beans.property.SimpleObjectProperty
 import javafx.beans.property.SimpleStringProperty
 import javafx.collections.FXCollections.observableArrayList
+import javafx.geometry.Insets
 import javafx.scene.control.TableView
 import javafx.scene.layout.BorderPane
 import me.dzikimlecz.lecturers.Lecturer
 import me.dzikimlecz.lecturers.SettlingPeriod
+import me.dzikimlecz.timetables.components.fragments.margin
 import tornadofx.*
 import java.time.LocalDate
 
@@ -14,9 +16,10 @@ class LecturerWorkTimeDisplay: View() {
     private val lecturers = observableArrayList<Lecturer>()
     private var table by singleAssign<TableView<Lecturer>>()
     private val filterStart = SimpleObjectProperty(LocalDate.now())
-    private val filterEnd = SimpleObjectProperty(LocalDate.now().minusDays(1))
-    private var timeFilter: (Map<SettlingPeriod, Int>) -> Map<SettlingPeriod, Int> = { it }
+    private val filterEnd = SimpleObjectProperty(LocalDate.now().plusDays(1))
+    private var timeFilter: (SettlingPeriod) -> Boolean = { true }
 
+    private val lecturerToTimeWorkedProperty = mutableMapOf<Lecturer, SimpleStringProperty>()
 
     override val root: BorderPane = borderpane {
         left {
@@ -32,55 +35,64 @@ class LecturerWorkTimeDisplay: View() {
                 }
                 column<Lecturer, String>("Czas Pracy") {
                     val lecturer = it.value
-                    SimpleStringProperty(lecturer.getHoursWorkedText())
+                    lecturer.updateHoursWorkedText()
+                    lecturerToTimeWorkedProperty[lecturer]!!
                 }
 
             }
         }
-        right {
-            form {
-                fieldset("Filtruj daty") {
-                    field("Początek") {
-                        datepicker(filterStart) {
+        right = form {
+            fieldset("Filtruj daty") {
+                field("Początek") {
+                    datepicker(filterStart) {
 
-                        }
                     }
-                    field("Koniec") {
-                        datepicker(filterEnd) {
+                }
+                field("Koniec") {
+                    datepicker(filterEnd) {
 
-                        }
                     }
-                    buttonbar {
-                        button("Ok") {
-                            isDefaultButton = true
-                            action {
-                                timeFilter = {
-                                    it.filterKeys { period ->
-                                        !period.end.isBefore(filterStart.get()) || !period.start.isAfter(filterEnd.get())
-                                    }
-                                }
-                                table.refresh()
+                }
+                buttonbar {
+                    button("Ok") {
+                        isDefaultButton = true
+                        action {
+                            timeFilter = {
+                                val filterRange = (filterStart.get())..(filterEnd.get())
+                                it.end in filterRange || it.start in filterRange
                             }
+                            lecturerToTimeWorkedProperty.keys.forEach { it.updateHoursWorkedText() }
+                            table.refresh()
                         }
                     }
                 }
             }
         }
+
+
+        left.margin = Insets(100.0, 100.0, 300.0, 500.0, )
+        right.margin = Insets(100.0, 500.0, 300.0, 100.0, )
     }
 
     fun refresh(lecturers: Collection<Lecturer>) {
-        timeFilter = { it }
+        timeFilter = { true }
         this.lecturers.clear()
         this.lecturers += lecturers
         filterStart.set(LocalDate.now())
-        filterEnd.set(LocalDate.now().minusDays(1))
+        filterEnd.set(LocalDate.now().plusDays(1))
     }
 
-    private fun Lecturer.getHoursWorkedText(): String {
-        val allMinutes = timeFilter(hoursWorked).values.sum()
+    private fun Lecturer.updateHoursWorkedText(): String {
+        val allMinutes = hoursWorked.filterKeys(timeFilter).values.sum()
         val hours = allMinutes / 60
         val minutes = allMinutes % 60
-        return "$hours h $minutes min"
+        val time = "$hours h $minutes min"
+        val timeWorkedProperty = lecturerToTimeWorkedProperty[this]
+        if (timeWorkedProperty != null) timeWorkedProperty.set(time)
+        else lecturerToTimeWorkedProperty[this] = SimpleStringProperty(time)
+        return time
     }
+
+
 
 }
